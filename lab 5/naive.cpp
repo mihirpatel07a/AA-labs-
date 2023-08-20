7d3b2212534971288938dfa5cbb88187e67d7cca
mihirpatel07a <105737077+mihirpatel07a@users.noreply.github.com>
2023-08-20 20:30:58 +0530

    Add files via upload

--- lab 5/naive.cpp
+++ lab 5/naive.cpp
@@ -1,1 +1,47 @@
+// Lab 5 aa
+
+
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    
+	string t, p;
+	cout<<"enter string:\n";
+	cin>>t;
+ 
+    
+   
+	cout<<"enter pattern string:\n";
+	cin>>p;
+    
+	int n = t.length();
+	int m = p.length();
+    
+	for(int s= 0; s<=n-m; s++)
+	{
+       int j = 0;
+    
+       while(j < m && t[s+j] == p[j])
+       {
+           j++;
+       }
+   	 
+    	if(j==m) {
+        	cout << s << endl;
+        	return 0;
+    	}
+    	 
+	}
+	cout<<"string not found"<<endl;
+    
+    
+    
+   
+
+	return 0;
+}
+
 
